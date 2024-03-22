6bf62cf2e782354807596e8116a064e510761ddb
aiferrydermawan <aiferrydermawan@gmail.com>
2024-03-22 11:13:41 +0700

    wip

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -65,7 +65,7 @@ class DashboardController extends Controller
     {
         $permohonan = Permohonan::query();
 
-        return $permohonan->where('tanggal_berakhir', '<', Carbon::now()->addMonth())
+        return $permohonan->doesntHave('dataPengiriman')
             ->whereMonth('tanggal_berakhir', '=', Carbon::now()->addMonth())
             ->whereYear('tanggal_berakhir', '=', Carbon::now()->year)
             ->count();
