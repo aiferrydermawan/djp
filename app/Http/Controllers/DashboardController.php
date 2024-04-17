c031200b718dc1e06111dcf1284453cbc06c1f78
aiferrydermawan <aiferrydermawan@gmail.com>
2024-04-17 22:13:50 +0700

    wip

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Permohonan;
+use App\Models\UserDetail;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -11,11 +12,15 @@ class DashboardController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $firstCard = $this->tunggakanBerkasKEBnNKEB();
-        $thirdCard = $this->berkasJatuhTempoBulanIni();
-        $fourthCard = $this->berkasJatuhTempoBulanDepan();
-        $firstChart = $this->tunggakanBerkas();
-        $secondChart = $this->permohonanMasuk();
+        $jabatan = UserDetail::select('jabatan')->where('user_id', auth()->user()->id)->first()->jabatan;
+        if (! $jabatan) {
+            abort(403, 'Anda belum mempunyai jabatan');
+        }
+        $firstCard = $this->tunggakanBerkasKEBnNKEB($jabatan);
+        $thirdCard = $this->berkasJatuhTempoBulanIni($jabatan);
+        $fourthCard = $this->berkasJatuhTempoBulanDepan($jabatan);
+        $firstChart = $this->tunggakanBerkas($jabatan);
+        $secondChart = $this->permohonanMasuk($jabatan);
 
         return inertia('Dashboard', [
             'firstCard' => $firstCard,
@@ -26,44 +31,111 @@ class DashboardController extends Controller
         ]);
     }
 
-    public function tunggakanBerkas()
+    public function tunggakanBerkas($jabatan)
     {
+        $user_id = auth()->user()->id;
+
         $permohonan = Permohonan::query()
             ->select('jenis_permohonan', DB::raw('COUNT(*) as total'))->groupBy('jenis_permohonan')
             ->with('jenisPermohonan')
             ->doesntHave('dataPengiriman');
 
+        if ($jabatan == 'penelaah keberatan') {
+            $permohonan->where('nama_pk', $user_id)->orWhere('nama_pk_2', $user_id);
+        }
+
+        if ($jabatan == 'kepala seksi') {
+            $permohonan->whereHas('penelaahKeberatan.detail', function ($query) use ($jabatan) {
+                $query->where('jabatan', $jabatan);
+            })->orWhereHas('penelaahKeberatan.detail', function ($query) use ($jabatan) {
+                $query->where('jabatan', $jabatan);
+            });
+        }
+
         return $permohonan->get();
     }
 
-    public function permohonanMasuk()
+    public function permohonanMasuk($jabatan)
     {
+        $user_id = auth()->user()->id;
+
         $permohonan = Permohonan::query()
             ->select('jenis_permohonan', DB::raw('COUNT(*) as total'))->groupBy('jenis_permohonan')
             ->with('jenisPermohonan');
 
+        if ($jabatan == 'penelaah keberatan') {
+            $permohonan->where('nama_pk', $user_id)->orWhere('nama_pk_2', $user_id);
+        }
+
+        if ($jabatan == 'kepala seksi') {
+            $permohonan->whereHas('penelaahKeberatan.detail', function ($query) use ($jabatan) {
+                $query->where('jabatan', $jabatan);
+            })->orWhereHas('penelaahKeberatan.detail', function ($query) use ($jabatan) {
+                $query->where('jabatan', $jabatan);
+            });
+        }
+
         return $permohonan->get();
     }
 
-    public function tunggakanBerkasKEBnNKEB()
+    public function tunggakanBerkasKEBnNKEB($jabatan)
     {
+        $user_id = auth()->user()->id;
         $permohonan = Permohonan::query();
+
+        if ($jabatan == 'penelaah keberatan') {
+            $permohonan->where('nama_pk', $user_id)->orWhere('nama_pk_2', $user_id);
+        }
+
+        if ($jabatan == 'kepala seksi') {
+            $permohonan->whereHas('penelaahKeberatan.detail', function ($query) use ($jabatan) {
+                $query->where('jabatan', $jabatan);
+            })->orWhereHas('penelaahKeberatan.detail', function ($query) use ($jabatan) {
+                $query->where('jabatan', $jabatan);
+            });
+        }
 
         return $permohonan->doesntHave('dataPengiriman')->count();
     }
 
-    public function berkasJatuhTempoBulanIni()
+    public function berkasJatuhTempoBulanIni($jabatan)
     {
+        $user_id = auth()->user()->id;
         $permohonan = Permohonan::query();
 
-        return $permohonan->doesntHave('dataPengiriman')->whereMonth('tanggal_berakhir', '=', Carbon::now()->month)
+        if ($jabatan == 'penelaah keberatan') {
+            $permohonan->where('nama_pk', $user_id)->orWhere('nama_pk_2', $user_id);
+        }
+
+        if ($jabatan == 'kepala seksi') {
+            $permohonan->whereHas('penelaahKeberatan.detail', function ($query) use ($jabatan) {
+                $query->where('jabatan', $jabatan);
+            })->orWhereHas('penelaahKeberatan.detail', function ($query) use ($jabatan) {
+                $query->where('jabatan', $jabatan);
+            });
+        }
+
+        $permohonan->doesntHave('dataPengiriman')->whereMonth('tanggal_berakhir', '=', Carbon::now()->month)
             ->whereYear('tanggal_berakhir', '=', Carbon::now()->year)
             ->count();
     }
 
-    public function berkasJatuhTempoBulanDepan()
+    public function berkasJatuhTempoBulanDepan($jabatan)
     {
+        $user_id = auth()->user()->id;
         $permohonan = Permohonan::query();
+
+        if ($jabatan == 'penelaah keberatan') {
+            $permohonan->where('nama_pk', $user_id)->orWhere('nama_pk_2', $user_id);
+        }
+
+        if ($jabatan == 'kepala seksi') {
+            $permohonan->whereHas('penelaahKeberatan.detail', function ($query) use ($jabatan) {
+                $query->where('jabatan', $jabatan);
+            })->orWhereHas('penelaahKeberatan.detail', function ($query) use ($jabatan) {
+                $query->where('jabatan', $jabatan);
+            });
+        }
 
         return $permohonan->doesntHave('dataPengiriman')
             ->whereMonth('tanggal_berakhir', '=', Carbon::now()->addMonth())
