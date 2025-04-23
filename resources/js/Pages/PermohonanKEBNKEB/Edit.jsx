b088d4dffd2c4f21445b7ecec9735df83936a581
Ferry Dermawan <aiferrydermawan@gmail.com>
2025-04-23 09:38:07 +0700

    wip

--- resources/js/Pages/PermohonanKEBNKEB/Edit.jsx
+++ resources/js/Pages/PermohonanKEBNKEB/Edit.jsx
@@ -3,7 +3,6 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link, router } from "@inertiajs/react";
 import Label from "../../Components/Label.jsx";
 import Input from "../../Components/Input.jsx";
-import NPWPInput from "../../Components/NPWPInput.jsx";
 import NOPInput from "../../Components/NOPInput.jsx";
 import Validation from "../../Components/Validation.jsx";
 import CustomCombobox from "../../Components/CustomCombobox.jsx";
@@ -29,7 +28,6 @@ const masaPajakAll = [
     "11",
     "12",
 ];
-const mataUangAll = ["rupiah", "dollar"];
 const formatNumber = (value) => {
     const numbers = value.toString().replace(/\D/g, ""); // Make sure value is a string
     return numbers.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
@@ -38,13 +36,11 @@ function Edit({
     errors,
     kpp_all,
     jenis_permohonan_all,
-    unit_yang_memproses_all,
     jenis_ketetapan_all,
     dasar_pemrosesan_all,
     pk_all,
     permohonan,
 }) {
-    const [jenisPajakAll, setJenisPajakAll] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
     const [namaWp, setNamaWp] = useState(permohonan.nama_wp);
@@ -58,33 +54,15 @@ function Edit({
             (item) => item.id === permohonan.jenis_permohonan,
         ),
     );
-    const [unitYangMemproses, setUnitYangMemproses] = useState(
-        unit_yang_memproses_all.find(
-            (item) => item.id === permohonan.unit_yang_memproses,
-        ),
-    );
-    const [jenisKetetapan, setJenisKetetapan] = useState(
-        jenis_ketetapan_all.find(
-            (item) => item.id === permohonan.jenis_ketetapan,
-        ),
-    );
     const [nomorKetetapan, setNomorKetetapan] = useState(
         permohonan.nomor_ketetapan,
     );
     const [tanggalKetetapan, setTanggalKetetapan] = useState(
         new Date(permohonan.tanggal_ketetapan),
     );
-    const [tanggalKirimKetetapan, setTanggalKirimKetetapan] = useState(
-        new Date(permohonan.tanggal_kirim_ketetapan),
-    );
-    const [jenisPajak, setJenisPajak] = useState(permohonan.jenis_pajak);
     const [masaPajak, setMasaPajak] = useState(permohonan.masa_pajak);
     const [tahunPajak, setTahunPajak] = useState(permohonan.tahun_pajak);
-    const [mataUang, setMataUang] = useState(permohonan.mata_uang);
     const [nilai1, setNilai1] = useState(formatNumber(permohonan.nilai_1));
-    const [nilai2, setNilai2] = useState(formatNumber(permohonan.nilai_2));
-    const [nilai3, setNilai3] = useState(formatNumber(permohonan.nilai_3));
-    const [nilai4, setNilai4] = useState(formatNumber(permohonan.nilai_4));
     const [dasarPemrosesan, setDasarPemrosesan] = useState(
         permohonan.dasar_pemrosesan,
     );
@@ -125,30 +103,10 @@ function Edit({
             : "",
     );
     const [namaPk2, setNamaPk2] = useState(permohonan.nama_pk_2 ?? "");
-    useEffect(() => {
-        if (jenisKetetapan != null) {
-            axios
-                .get("/api/jenis-pajak", {
-                    params: { jenis_ketetapan_id: jenisKetetapan },
-                })
-                .then((response) => {
-                    setJenisPajakAll(response.data);
-                    setJenisPajak(permohonan.jenis_pajak);
-                })
-                .catch((error) => {
-                    console.error("There was an error!", error);
-                });
-        } else {
-            setJenisPajakAll([]);
-        }
-    }, [jenisKetetapan]);
     const edit = async (e) => {
         e.preventDefault();
         const tanggal_ketetapan = tanggalKetetapan
             ? tanggalKetetapan.toLocaleDateString("en-CA")
-            : null;
-        const tanggal_kirim_ketetapan = tanggalKirimKetetapan
-            ? tanggalKirimKetetapan.toLocaleDateString("en-CA")
             : null;
         const tanggal_surat_wp = tanggalSuratWp
             ? tanggalSuratWp.toLocaleDateString("en-CA")
@@ -177,21 +135,11 @@ function Edit({
             nop: nop,
             kode_kpp_terdaftar: kpp ? kpp.id : null,
             jenis_permohonan: jenisPermohonan ? jenisPermohonan.id : null,
-            unit_yang_memproses: unitYangMemproses
-                ? unitYangMemproses.id
-                : null,
-            jenis_ketetapan: jenisKetetapan ? jenisKetetapan.id : null,
             nomor_ketetapan: nomorKetetapan,
             tanggal_ketetapan: tanggal_ketetapan,
-            tanggal_kirim_ketetapan: tanggal_kirim_ketetapan,
-            jenis_pajak: jenisPajak,
             masa_pajak: masaPajak,
             tahun_pajak: tahunPajak,
-            mata_uang: mataUang,
             nilai_1: nilai1,
-            nilai_2: nilai2,
-            nilai_3: nilai3,
-            nilai_4: nilai4,
             dasar_pemrosesan: dasarPemrosesan,
             nomor_surat_wp: nomorSuratWp,
             tanggal_surat_wp: tanggal_surat_wp,
@@ -258,10 +206,9 @@ function Edit({
                                         className={`form-control col-span-2`}
                                     >
                                         <Label name="NPWP" />
-                                        <NPWPInput
-                                            value={npwp}
-                                            onChange={(formattedValue) =>
-                                                setNpwp(formattedValue)
+                                        <Input
+                                            onChange={(e) =>
+                                                setNpwp(e.target.value)
                                             }
                                             placeholder="Type Here"
                                         />
@@ -326,40 +273,6 @@ function Edit({
                                     <label
                                         className={`form-control col-span-2`}
                                     >
-                                        <Label name="Unit yang Memproses" />
-                                        <CustomCombobox
-                                            selected={unitYangMemproses}
-                                            onChange={setUnitYangMemproses}
-                                            placeholder="Kosong"
-                                            items={unit_yang_memproses_all}
-                                        />
-                                        {errors.unit_yang_memproses && (
-                                            <Validation>
-                                                {errors.unit_yang_memproses}
-                                            </Validation>
-                                        )}
-                                    </label>
-                                    <div className={`col-span-2`}></div>
-                                    <label
-                                        className={`form-control col-span-2`}
-                                    >
-                                        <Label name="Jenis Ketetapan" />
-                                        <CustomCombobox
-                                            selected={jenisKetetapan}
-                                            onChange={setJenisKetetapan}
-                                            placeholder="Kosong"
-                                            items={jenis_ketetapan_all}
-                                        />
-                                        {errors.jenis_ketetapan && (
-                                            <Validation>
-                                                {errors.jenis_ketetapan}
-                                            </Validation>
-                                        )}
-                                    </label>
-                                    <div className={`col-span-2`}></div>
-                                    <label
-                                        className={`form-control col-span-2`}
-                                    >
                                         <Label name="Nomor Ketetapan" />
                                         <NoKetetapanInput
                                             value={nomorKetetapan}
@@ -394,54 +307,6 @@ function Edit({
                                             </Validation>
                                         )}
                                     </label>
-                                    <label
-                                        className={`form-control col-span-1`}
-                                    >
-                                        <Label name="Tanggal Kirim Ketetapan" />
-                                        <DatePicker
-                                            placeholderText="kosong"
-                                            className="input input-bordered"
-                                            selected={tanggalKirimKetetapan}
-                                            onChange={(date) =>
-                                                setTanggalKirimKetetapan(date)
-                                            }
-                                            dateFormat="dd-MM-yyyy"
-                                        />
-                                        {errors.tanggal_kirim_ketetapan && (
-                                            <Validation>
-                                                {errors.tanggal_kirim_ketetapan}
-                                            </Validation>
-                                        )}
-                                    </label>
-                                    <label
-                                        className={`form-control col-span-2`}
-                                    >
-                                        <Label name="Jenis Pajak" />
-                                        <Select
-                                            value={jenisPajak}
-                                            onChange={(e) =>
-                                                setJenisPajak(e.target.value)
-                                            }
-                                        >
-                                            <option value="">Pilih 1</option>
-                                            {jenisPajakAll.map(
-                                                (item, index) => (
-                                                    <option
-                                                        key={index}
-                                                        value={item.id}
-                                                    >
-                                                        {item.name}
-                                                    </option>
-                                                ),
-                                            )}
-                                        </Select>
-                                        {errors.jenis_pajak && (
-                                            <Validation>
-                                                {errors.jenis_pajak}
-                                            </Validation>
-                                        )}
-                                    </label>
-                                    <div className="col-span-2"></div>
                                     <label
                                         className={`form-control col-span-2`}
                                     >
@@ -489,33 +354,6 @@ function Edit({
                                     <label
                                         className={`form-control col-span-2`}
                                     >
-                                        <Label name="Mata Uang" />
-                                        <Select
-                                            value={mataUang}
-                                            onChange={(e) =>
-                                                setMataUang(e.target.value)
-                                            }
-                                        >
-                                            <option value="">Pilih 1</option>
-                                            {mataUangAll.map((item, index) => (
-                                                <option
-                                                    key={index}
-                                                    value={item}
-                                                >
-                                                    {item}
-                                                </option>
-                                            ))}
-                                        </Select>
-                                        {errors.mata_uang && (
-                                            <Validation>
-                                                {errors.mata_uang}
-                                            </Validation>
-                                        )}
-                                    </label>
-                                    <div className="col-span-2"></div>
-                                    <label
-                                        className={`form-control col-span-2`}
-                                    >
                                         <Label name="Nilai Ketetapan (SKP / STP)" />
                                         <NumberInput
                                             value={nilai1}
@@ -527,61 +365,6 @@ function Edit({
                                         {errors.nilai_1 && (
                                             <Validation>
                                                 {errors.nilai_1}
-                                            </Validation>
-                                        )}
-                                    </label>
-                                    <div className="col-span-2"></div>
-                                    <label
-                                        className={`form-control col-span-2`}
-                                    >
-                                        <Label name="Nilai Sanksi Administrasi" />
-                                        <NumberInput
-                                            value={nilai2}
-                                            placeholder="Kosong"
-                                            onChange={(formattedValue) =>
-                                                setNilai2(formattedValue)
-                                            }
-                                        />
-                                        {errors.nilai_2 && (
-                                            <Validation>
-                                                {errors.nilai_2}
-                                            </Validation>
-                                        )}
-                                    </label>
-                                    <div className="col-span-2"></div>
-                                    <label
-                                        className={`form-control col-span-2`}
-                                    >
-                                        <Label name="Nilai Ketetapan / Sanksi Administrasi yang disetujui" />
-                                        <NumberInput
-                                            value={nilai3}
-                                            type="text"
-                                            placeholder="Kosong"
-                                            onChange={(formattedValue) =>
-                                                setNilai3(formattedValue)
-                                            }
-                                        />
-                                        {errors.nilai_3 && (
-                                            <Validation>
-                                                {errors.nilai_3}
-                                            </Validation>
-                                        )}
-                                    </label>
-                                    <div className="col-span-2"></div>
-                                    <label
-                                        className={`form-control col-span-2`}
-                                    >
-                                        <Label name="Nilai Ajukan Upaya Hukum" />
-                                        <NumberInput
-                                            value={nilai4}
-                                            placeholder="Kosong"
-                                            onChange={(formattedValue) =>
-                                                setNilai4(formattedValue)
-                                            }
-                                        />
-                                        {errors.nilai_4 && (
-                                            <Validation>
-                                                {errors.nilai_4}
                                             </Validation>
                                         )}
                                     </label>
