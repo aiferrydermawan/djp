ca18faef4c9757cb1989c6f0f497b30c1c1d6ebe
Ferry Dermawan <aiferrydermawan@gmail.com>
2025-06-12 16:36:03 +0700

    wip

--- resources/views/livewire/monitoring/penyelesaian-permohonan.blade.php
+++ resources/views/livewire/monitoring/penyelesaian-permohonan.blade.php
@@ -1,3 +1,42 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+<div class="p-5">
+    <div class="breadcrumbs text-sm">
+        <ul>
+            <li>Penyelesaian Permohonan</li>
+        </ul>
+    </div>
+    <div class="card mt-5 bg-base-100 shadow">
+        <div class="card-body">
+            <div class="card-title"></div>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Uraian</th>
+                    <th>Tahun</th>
+                    <th>Seksi</th>
+                    <th>Saldo Awal</th>
+                    <th>Permohonan Masuk Tahun Ini</th>
+                    <th>Selesai</th>
+                    <th>Saldo Akhir</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Keberatan</td>
+                </tr>
+                <tr>
+                    <td>Non Keberatan</td>
+                </tr>
+                <tr>
+                    <td>SUB</td>
+                </tr>
+                <tr>
+                    <td>STG</td>
+                </tr>
+                <tr>
+                    <th>Jumlah</th>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
