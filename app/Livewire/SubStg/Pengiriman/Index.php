af38b799bde39a990df6e874913ebe00fb3d3c3a
Ferry Dermawan <aiferrydermawan@gmail.com>
2025-07-03 17:39:32 +0700

    wip

--- app/Livewire/SubStg/Pengiriman/Index.php
+++ app/Livewire/SubStg/Pengiriman/Index.php
@@ -16,6 +16,11 @@ class Index extends Component
     {
         $permintaan = Permintaan::query()->with(['penelaahKeberatan.detail.organisasi', 'pengiriman'])->has('suratJawaban');
 
+        if($this->search){
+            $permintaan->where('npwp', 'like', '%'.$this->search.'%')
+            ->orWhere('nomor_surat_pp', 'like', '%'.$this->search.'%');
+        }
+
         return view('livewire.sub-stg.pengiriman.index', [
             'permintaan' => $permintaan->latest()->paginate(10),
         ]);
